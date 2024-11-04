15f1af9c830b9dfaf2f51064ddd974f903178f54
IDFCSGFY <idfcsgfy@gmail.com>
2024-11-05 06:09:48 +0700

    initial commit

---
+++ src/main/java/HW/CalculatorForTests/CalculatorForTestsApplication.java
@@ -0,0 +1,13 @@
+package HW.CalculatorForTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorForTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorForTestsApplication.class, args);
+	}
+
+}
